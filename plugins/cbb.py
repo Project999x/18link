f927d7eb7f4c8725a411cef6d5f57d6b5349615f
KX3M <163962831+KX3M@users.noreply.github.com>
2025-06-21 13:28:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -4,6 +4,20 @@ from config import OWNER_ID, ABOUT_TXT, HELP_TXT, START_MSG
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from database.database import add_user, del_user, full_userbase, present_user
 
+message_content = '''👋 Hey {first}\n
+🎖️ Available Plans :\n
+<blockquote>● 30 rs For 7 Days Prime Membership\n
+● 110 rs For 1 Month Prime Membership\n
+● 299 rs For 3 Months Prime Membership\n
+● 550 rs For 6 Months Prime Membership\n
+● 999 rs For 1 Year Prime Membership</blockquote>\n\n
+💵 UPI ID - <code>trustable@upi </code>\n
+<b>(Tap to copy UPI Id)</b>\n\n
+📸 QR - <a href="https://graph.org/file/ac61481c6c90015545d83-6b573a858fa21d40c6.jpg">ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ</a>\n\n
+♻️ <b>If payment is not getting sent on above given QR code then inform admin, he will give you new QR code</b>\n\n
+‼️ ᴍᴜsᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ'''
+
+
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
@@ -42,8 +56,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             ])
         )
     elif data == "premium":
-        await query.message.edit_text(
-            text=f"Premium Benefits & Perks\nDirect Channel Links, No Ad Links\nSpecial Access In Events\n\n<blockquote expandable>Pricing Rates\n7 Days - INR 39\n15 Days - INR 69\n1 Month - INR 89\n3 Months - INR 249\n6 Months - INR 479\n12 Month - Contact <a href=https://t.me/Sorryfucked>Owner</a></blockquote>\n\n<blockquote>Want To Buy?\nUsing UPI <code>trustable@upi</code>\nSend Screenshot to <a href=https://t.me/Sorryfucked>Owner</a></blockquote>\n\nWe Have Limited Seats For Premium Users",
+        await query.message.reply_photo(
+            photo=("https://graph.org/file/ac61481c6c90015545d83-6b573a858fa21d40c6.jpg"),
+            caption=message_content.format(
+                first = query.from_user.mention, 
+                second = query.from_user.mention
+            ),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
