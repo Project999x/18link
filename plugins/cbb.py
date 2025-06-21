9a27ee8b7725f18547fff0871e5f455b9ed22ca9
KX3M <163962831+KX3M@users.noreply.github.com>
2025-06-21 16:18:07 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,15 +5,15 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from database.database import add_user, del_user, full_userbase, present_user
 
 message_content = '''👋 <b>Hey {first}\n
-🎖️<u> Available Plans </u>:</b>\n
+🎖️ <u>Available Plans</u>:</b>\n
 <blockquote expandable><i>● 30 rs For 7 Days Prime Membership\n
 ● 110 rs For 1 Month Prime Membership\n
 ● 299 rs For 3 Months Prime Membership\n
 ● 550 rs For 6 Months Prime Membership\n
-● 999 rs For 1 Year Prime Membership</i></blockquote>\n\n
-💵 UPI ID - <code>trustable@upi</code>\n
+● 999 rs For 1 Year Prime Membership</i></blockquote>\n
+💵 UPI ID - <code>trustable@upi</code>
 <b>(Tap to copy UPI Id)</b>\n\n
-♻️ <b>If payment is not getting sent on above given QR code then inform Admin, He will give you new QR code</b>\n\n
+♻️ <b>If payment is not getting sent on above given QR code then inform Admin, He will give you new QR code</b>\n
 ‼️ ᴍᴜsᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ'''
 
 
